621238a59e6cff4bba0f86bb4bcfa9801f9285e5
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-03-27 06:07:00 -0400

    Example app knocked out

---
+++ src/containers/App/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { FormContainer } from "../FormContainer";
+import { SinglePokemonContainer } from "../SinglePokemonContainer";
+import * as AppStyles from "./index.styles";
+import { Global, css } from "@emotion/core";
+
+const GlobalStyles = css`
+  html,
+  body {
+    background-color: #eb2f06;
+    font-size: calc(14px + (26 - 14) * ((100vw - 300px) / (1920 - 300)));
+  }
+`;
+
+const validateIsDisabled = inputValue => (inputValue === "" ? true : false);
+
+const App = () => {
+  const [searchInputValue, onSearchInputChange] = useState("");
+  const [submittedValueState, onFormSubmit] = useState(null);
+  const isDisabled = validateIsDisabled(searchInputValue);
+  const handleChange = event => onSearchInputChange(event.target.value);
+  // BUGFIX: when you clear the input it resubmits with an empty string.
+  // BUGFIX: when you have a typo, it errors out
+  const handleSubmit = event => {
+    event.preventDefault();
+    onFormSubmit(searchInputValue);
+  };
+
+  return (
+    <AppStyles.Layout className="App">
+      <Global styles={GlobalStyles} />
+      <FormContainer
+        {...{
+          handleChange,
+          handleSubmit,
+          isDisabled,
+          searchInputValue,
+        }}
+      />
+      <AppStyles.CharacterDisplay>
+        {submittedValueState ? (
+          <SinglePokemonContainer pokemonName={searchInputValue} />
+        ) : null}
+      </AppStyles.CharacterDisplay>
+    </AppStyles.Layout>
+  );
+};
+
+export default App;
---
+++ src/containers/SinglePokemonContainer/index.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { PokemonCard } from "../../components/PokemonCard";
+
+export async function fetchPokemon(
+  pokemonName,
+  loadingChange,
+  pokemonLoadChange
+) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  console.log(url);
+  const { data: result } = await axios(url);
+  pokemonLoadChange(result);
+  loadingChange(false);
+}
+
+export const SinglePokemonContainer = ({ pokemonName }) => {
+  const [isLoading, onLoadingStateChange] = useState(true);
+  const [pokemon, onPokemonDataLoad] = useState({});
+  useEffect(() => {
+    fetchPokemon(pokemonName, onLoadingStateChange, onPokemonDataLoad);
+  }, [pokemonName]);
+  return isLoading ? <div>Loading</div> : <PokemonCard {...{ pokemon }} />;
+};
+
+SinglePokemonContainer.propTypes = {
+  pokemonName: PropTypes.string.isRequired,
+};
